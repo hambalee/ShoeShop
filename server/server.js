0e0b50af7e233cb40939bb58058c0550b595a349
hambalee <hambalee@outlook.com>
2022-09-20 22:23:10 +0700

    can get products data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import products from './data/Products.js'
 
 const app = express()
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
 
 app.get('/', (req, res) => {
     res.send("API is running ...")
