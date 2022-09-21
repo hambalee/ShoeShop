61a0811f29f3c81bf95128573a9bfefafa4d0390
hambalee <hambalee@outlook.com>
2022-09-21 19:52:03 +0700

    can get single product

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ app.get('/api/products', (req, res) => {
     res.json(products)
 })
 
+app.get('/api/product/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+
 app.get('/', (req, res) => {
     res.send("API is running ...")
 })
